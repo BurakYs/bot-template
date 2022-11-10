bc3f8930a7ab3461c49778a2654c0b8a132cf824
Burak <59135244+BurakYs@users.noreply.github.com>
2022-11-10 21:56:02 +0300

    Delete messagecommand.js

--- loaders/messagecommand.js
+++
@@ -1,9 +0,0 @@
-const { glob } = require('glob');
-const { promisify } = require('util');
-module.exports = async (client) => {
-    const commandss = await promisify(glob)("./mcommands/*/*.js");
-    commandss.map((value) => {
-        let file = require(`.${value}`);
-        client.mcommands.set(file.command.name, file);
-    });
-};
