6ab58c0a2600cee38bb52405e90cf1010d18d929
Burak <59135244+BurakYs@users.noreply.github.com>
2022-11-10 22:11:03 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,17 +8,23 @@ module.exports = {
         const command = message.content.split(" ")[0].slice(prefix.length)
         let args = message.content.split(" ")
         args.shift()
-        const clientcmd = client.mcommands.get(command)
-        if(!clientcmd) return;
-        const memberperm = clientcmd.command.memberPerm.replace("Administrator", "Administrator").replace("Administrator", "Yönetici").replace("ManageChannels", "Kanalları Yönet").replace("ManageRoles", "Rolleri Yönet").replace("ManageMessages", "Mesajları Yönet").replace("ManageGuild", "Sunucuyu Yönet").replace("ModerateMembers", "Üyelere Zamanaşımı Uygula").replace("BanMembers", "Üyeleri Yasakla").replace("KickMembers", "Üyeleri At")
-        const botperm = clientcmd.command.botPerm.replace("Administrator", "Yönetici").replace("ManageChannels", "Kanalları Yönet").replace("ManageRoles", "Rolleri Yönet").replace("ManageMessages", "Mesajları Yönet").replace("ManageGuild", "Sunucuyu Yönet").replace("ModerateMembers", "Üyelere Zamanaşımı Uygula").replace("BanMembers", "Üyeleri Yasakla").replace("KickMembers", "Üyeleri At")
-        if (clientcmd.command.memberPerm !== "NOPERM") {
-            if (!message.member.permissions.has(clientcmd.command.memberPerm)) return message.reply({ embeds: [new EmbedBuilder().setTitle("Hata").setColor(resolveColor("Red")).setDescription(`\`${memberperm}\` iznin yok.`)] })
+        const clientcmd = client.messagecmds.get(command)
+        if (!clientcmd) return;
+        const cmd = client.commands.find(x => x.name === clientcmd.command.name)
+        if (!message.guild && cmd.dm === false) return;
+        const reqPermMember = cmd.reqPermMember.replace("Administrator", "Administrator").replace("ManageChannels", "ManageChannels").replace("ManageRoles", "Manage Roles").replace("ManageMessages", "Manage Messages").replace("ManageGuild", "Manage Server").replace("ModerateMembers", "Timeout Members").replace("BanMembers", "Ban Members").replace("KickMembers", "Kick Members").replace("ManageNicknames", "Manage Nicknames")
+        const reqPermBot = cmd.reqPermBot.replace("Administrator", "Administrator").replace("ManageChannels", "ManageChannels").replace("ManageRoles", "Manage Roles").replace("ManageMessages", "Manage Messages").replace("ManageGuild", "Manage Server").replace("ModerateMembers", "Timeout Members").replace("BanMembers", "Ban Members").replace("KickMembers", "Kick Members").replace("ManageNicknames", "Manage Nicknames")
+        if (cmd.reqPermMember !== "NONE") {
+            if (!message.member.permissions.has(cmd.reqPermMember)) return message.reply({ embeds: [new EmbedBuilder().setTitle("Missing Permissions").setColor(resolveColor("Red")).setDescription(`You don't have this permission: \`${reqPermMember}\``)] })
         }
-        if (clientcmd.command.botPerm !== "NOPERM") {
-            if (!message.guild.members.me.permissions.has(clientcmd.command.botPerm)) return message.reply({ embeds: [new EmbedBuilder().setTitle("Hata").setColor(resolveColor("Red")).setDescription(`\`${botperm}\` iznim yok.`)] })
+        if (cmd.reqPermBot !== "NONE") {
+            if (!message.guild.members.me.permissions.has(cmd.reqPermBot)) return message.reply({ embeds: [new EmbedBuilder().setTitle("Missing Permissions").setColor(resolveColor("Red")).setDescription(`I don't have this permission: \`${reqPermBot}\``)] })
         }
-        if(clientcmd.command.developer === true && client.config.owners.includes(message.author.id)) {return;}
-        clientcmd.run({ client, message, args })
+        try {
+            clientcmd.run({ client, message, args, prefix })
+        } catch (error) {
+            return console.log(error)
+
+        }
     }
 }
