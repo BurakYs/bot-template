d4b5afd66c296d391e2c316c900e1ecb54f45d87
Burak <59135244+BurakYs@users.noreply.github.com>
2022-11-10 21:57:57 +0300

    Update command.js

--- loaders/command.js
+++ loaders/command.js
@@ -1,16 +1,27 @@
 const { glob } = require('glob');
 const { promisify } = require('util');
 module.exports = async (client) => {
-    const commandss = await promisify(glob)("./commands/*/*.js");
+    const folder = await promisify(glob)("./commands/slash/*/*.js");
     const commands = [];
-    commandss.map((value) => {
+    folder.map((value) => {
         let file = require(`.${value}`);
-        commands.push(file.data.toJSON());
-        client.commands.set(file.data.name, file);
+        commands.push(file.data.toJSON())
+        client.slashcmds.set(file.data.name, file);
+    });
+    const folder2 = await promisify(glob)("./commands/context/*/*.js");
+    folder2.map((value) => {
+        let file = require(`.${value}`);
+        commands.push(file.data.toJSON())
+        client.contextcmds.set(file.data.name, file);
+    });
+    const folder3 = await promisify(glob)("./commands/message/*/*.js");
+    folder3.map((value) => {
+        let file = require(`.${value}`);
+        client.messagecmds.set(file.command.name, file);
     });
     client.once('ready', async () => {
         try {
-            client.application.commands.set(commands)
+            client.application.commands.set(commands).then(console.log("Global")).catch(e => console.log(e))
         } catch (error) {
             console.log(error)
         }
