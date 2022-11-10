1933f103acbb615baad8feaf627f206c4bbcdb25
Burak <59135244+BurakYs@users.noreply.github.com>
2022-11-10 22:05:48 +0300

    Update ping.js

--- commands/slash/ping.js
+++ commands/slash/ping.js
@@ -1,1 +1,9 @@
-
+const { SlashCommandBuilder } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription("Ping Pong!"),
+    async run(interaction) {
+        interaction.reply({ content: `${client.ws.ping}ms` })
+    }
+}
