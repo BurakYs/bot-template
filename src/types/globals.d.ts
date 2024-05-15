67aa022c6ffbffc1e1c1fda2338a274b1497fdf0
Burak <59135244+BurakYs@users.noreply.github.com>
2024-05-16 02:31:50 +0300

    Remove extra global var

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,14 +1,13 @@
 /* eslint-disable no-var */
 import { Logger } from '@/helpers/classes';
-import Base from '@/loaders/base';
+import Client from '@/loaders/base';
 
 declare global {
     var logger: Logger;
-    var client: Base;
-    var commands: any[];
+    var client: Client;
 
     interface String {
         title(language?: string): string;
-        change(replacements?: { [key: string]: any }): string;
+        change(replacements: { [key: string]: any }): string;
     }
 }
