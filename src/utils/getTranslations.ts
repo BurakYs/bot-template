659228b3f47282b9e6e2a6b082adbc5d2e73ecaf
BurakYs <brkyhs0@gmail.com>
2025-01-30 13:14:32 +0300

    Support Locale in first parameter in getTranslations

--- src/utils/getTranslations.ts
+++ src/utils/getTranslations.ts
@@ -1,5 +1,6 @@
-import config from '@/config';
 import JSONDatabase from '@/utils/classes/JSONDatabase';
+import config from '@/config';
+
 import type { Interaction } from 'discord.js';
 
 const languages = Object.entries(config.bot.supportedLanguages).reduce<Record<string, JSONDatabase>>((acc, [key, value]) => {
@@ -7,10 +8,14 @@ const languages = Object.entries(config.bot.supportedLanguages).reduce<Record<st
   return acc;
 }, {});
 
-export default function getTranslations(interaction: Interaction, path: string): any {
-  const { defaultLanguage } = config.bot;
+const { defaultLanguage } = config.bot;
+
+export default function getTranslations(interactionOrLanguage: Interaction | Interaction['language'], path: string) {
+  const language = typeof interactionOrLanguage === 'object' ? interactionOrLanguage.language || interactionOrLanguage.locale : interactionOrLanguage;
+
   const defaultTranslations = languages[defaultLanguage].get(path);
-  const translations = (languages[interaction.language || interaction.locale] || languages[defaultLanguage]).get(path);
+  const languageFile = languages[language] || languages[defaultLanguage];
+  const translations = languageFile.get(path);
 
   return translations || defaultTranslations;
 }
