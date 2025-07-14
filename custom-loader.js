b5ed3eb43b97998322c2fdd2a861a3208f95fa07
BurakYs <brkyhs0@gmail.com>
2025-07-14 11:54:07 +0300

    Simplify custom-loader.js

--- custom-loader.js
+++ custom-loader.js
@@ -1,55 +1,37 @@
-import 'dotenv/config.js';
 import fs from 'node:fs';
 import path from 'node:path';
 
-const base = process.cwd();
+const extensions = ['.ts', '/index.ts', '.json', ''];
 
-const tsconfigPath = path.resolve(base, 'tsconfig.json');
-const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'));
+const srcPath = path.resolve(process.cwd(), 'src');
+const srcPrefix = '@/';
 
-const baseUrl = tsconfig.compilerOptions?.baseUrl || '.';
-const paths = tsconfig.compilerOptions?.paths || {};
+function resolvePath(specifier) {
+    const filePath = specifier.slice(srcPrefix.length);
 
-const aliases = Object.entries(paths).reduce((acc, [key, values]) => {
-    acc[key] = values.map((v) => path.resolve(base, baseUrl, v));
-    return acc;
-}, {});
-
-function matchAlias(specifier) {
-    for (const aliasPattern of Object.keys(aliases)) {
-        if (aliasPattern.includes('*')) {
-            const regexPattern = aliasPattern.replace(/[.+^${}()|[\]\\]/g, '\\$&').replace(/\*/g, '(.*)');
-            const regex = new RegExp(`^${regexPattern}$`);
-            const match = specifier.match(regex);
-
-            if (match) {
-                return { aliasPattern, wildcardMatch: match[1] };
+    for (const ext of extensions) {
+        const fullPath = path.resolve(srcPath, filePath + ext);
+        if (fs.existsSync(fullPath)) {
+            if (!fs.statSync(fullPath).isDirectory()) {
+                return `file://${fullPath}`;
             }
-        } else if (specifier === aliasPattern) {
-            return { aliasPattern, wildcardMatch: null };
         }
     }
 
     return null;
 }
 
-export const resolve = (specifier, parentModuleURL, defaultResolve) => {
-    const matched = matchAlias(specifier);
-    if (!matched) return defaultResolve(specifier, parentModuleURL);
+export function resolve(specifier, context, nextResolve) {
+    if (specifier.startsWith('@/')) {
+        const resolved = resolvePath(specifier);
 
-    const { aliasPattern, wildcardMatch } = matched;
-    const targetPaths = aliases[aliasPattern];
-    let newSpecifier = wildcardMatch !== null ? targetPaths[0].replace(/\*/g, wildcardMatch) : targetPaths[0];
-
-    if (specifier.startsWith('@/') && !['ts', 'json'].includes(newSpecifier.split('.').pop())) {
-        try {
-            if (fs.statSync(newSpecifier).isDirectory()) {
-                newSpecifier = path.join(newSpecifier, 'index.ts');
-            }
-        } catch {
-            newSpecifier = `${newSpecifier}.ts`;
+        if (resolved) {
+            return {
+                url: resolved,
+                shortCircuit: true
+            };
         }
     }
 
-    return defaultResolve(`file://${newSpecifier}`, parentModuleURL);
-};
+    return nextResolve(specifier, context);
+}
